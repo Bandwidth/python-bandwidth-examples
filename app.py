35a0648d3eaa64cfcb4e34921856f6c76a3840c2
Sasha Myasoedov <smyasoedov@cybervisiontech.com>
2014-09-18 14:57:52 +0300

    Fixed bug in url

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def handle_event():
             elif event.tag == 'gather_complete':
                 bridge = Bridge.create(call)
                 bridge.call_party(CALLER, BRIDGE_CALLEE,
-                                  callback_url=APP_CALL_URL,
+                                  callback_url='http://{}{}'.format(DOMAIN, '/events/bridged'),
                                   tag='bridge-id:{}'.format(bridge.id))
 
     elif isinstance(event, GatherCallEvent):
